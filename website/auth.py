d03b26e2a381ba02982de4e1116e17502f58db24
ryantanjw <reagona778@gmail.com>
2025-06-15 00:30:45 +0800

    basic POST testing

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,request
+from flask import Blueprint,render_template,request,redirect,url_for
 
 auth = Blueprint('auth',__name__,static_folder='static')
 
@@ -7,8 +7,16 @@ auth = Blueprint('auth',__name__,static_folder='static')
 
 review_sentiment_per=0.0
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])  # Make sure to allow both GET and POST methods
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        print(f"Email: {email}")
+        print(f"Password: {password}")
+        return redirect(url_for('views.home'))  # Replace 'auth.dashboard' with the name of the route you want to redirect to
+
     return render_template("login.html")
     
 
