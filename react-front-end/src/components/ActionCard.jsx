e1a8170da834ce766d05db9d21c1edb5a2eca50d
marcusayay <marcusayay@gmail.com>
2025-06-16 02:39:52 +0800

    Component Experiment + Responsive Font Sizing Update

--- react-front-end/src/components/ActionCard.jsx
+++ react-front-end/src/components/ActionCard.jsx
@@ -14,7 +14,7 @@ export default function ActionCard({
       {/* Header and icon */}
       <div className="flex justify-between items-start">
         <div>
-          <h5 className="text-lg  xl:text-xl  font-semibold">{header}</h5>
+          <h5 className="text-base sm:text-lg xl:text-xl font-semibold">{header}</h5>
           <p className="mt-2 text-base text-gray-600">{subtext}</p>
         </div>
         <div className="text-3xl">
@@ -23,10 +23,10 @@ export default function ActionCard({
       </div>
 
       {/* Start button */}
-      <div className="mt-6 flex justify-center">
+      <div className="mt-4 flex justify-center">
         <button
           onClick={onStart}
-          className="px-6 py-2 bg-gray-200 font-medium text-black rounded-full hover:bg-gray-300 focus:outline-none"
+          className="px-6 py-2 bg-gray-200 font-medium text-black rounded-full hover:bg-gray-300 focus:outline-none text-sm sm:text-base"
         >
           {startText}
         </button>
