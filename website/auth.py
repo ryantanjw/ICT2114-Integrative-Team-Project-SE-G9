a4d655632f14d2306d9e44a58f7d21ac194ea75b
ryantanjw <reagona778@gmail.com>
2025-06-15 11:10:46 +0800

    Added Login via matching credentials - hashing not included yet

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint,render_template,request,redirect,url_for
+from models import db, User
 
 auth = Blueprint('auth',__name__,static_folder='static')
 
@@ -13,10 +14,32 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        print(f"Email: {email}")
-        print(f"Password: {password}")
-        return redirect(url_for('views.home'))  # Replace 'auth.dashboard' with the name of the route you want to redirect to
+        try:
+            #SELECT WHERE
+            user = User.query.filter_by(user_email=email).first()
+
+            if user:
+                if (user.password == password):
+                    return redirect(url_for('views.home'))  # Replace 'auth.dashboard' with the name of the route you want to redirect to
+                else:
+                    #TODO: Error Handling for invalid password
+                    return "Invalid password"
+            else:
+                #TODO: Error Handling for unknown User
+                #This will return an error and tell the user invalid email n pw
+                return "User not found"
+            
+        except Exception as e:
+            return f"Error: {str(e)}"
 
     return render_template("login.html")
-    
 
+# Example of connecting to the DB --> this example works [TO BE REMOVED AFTER TESTING]
+# @views.route('/')
+# def index():
+#     try:
+#         # Query the database (just a simple example)
+#         user = User.query.first()  # Fetch the first user in the DB
+#         return f'Hello, {user.user_name}!' if user else 'No user found.'
+#     except Exception as e:
+#         return f"Error connecting to DB: {str(e)}"
