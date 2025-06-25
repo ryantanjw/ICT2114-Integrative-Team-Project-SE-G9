ad9aeacf1112f4aac12059a165a42d194b145437
ryantanjw <reagona778@gmail.com>
2025-06-25 18:29:34 +0800

    Minor Fix

--- models.py
+++ models.py
@@ -25,7 +25,7 @@ class Form(db.Model):
     location = db.Column(db.String(255), nullable=True)
     title = db.Column(db.String(124), nullable=True)
     division = db.Column(db.String(124), nullable=True)
-    process = db.Column(db.models.TextField(("")), nullable=True)
+    process = db.Column(db.Text, nullable=True)
     approval = db.Column(db.Integer, default=False, nullable=True)
     last_access_date = db.Column(db.DateTime, nullable=True)
     last_review_date = db.Column(db.DateTime, nullable=True)
@@ -40,15 +40,15 @@ class Hazard(db.Model):
     hazard = db.Column(db.String(124), nullable=False)
     injury = db.Column(db.String(124), nullable=False)
     hazard_type = db.Column(db.String(255), nullable=True)
-    remarks = db.Column(db.models.TextField(("")), nullable=True)
+    remarks = db.Column(db.Text, nullable=True)
     
 class Risk(db.Model):
     __tablename__ = 'risk'
 
     # Define fields
     risk_id = db.Column(db.Integer, primary_key=True)
-    existing_risk_control = db.Column(db.models.TextField(("")), nullable=True)
-    additional_risk_control = db.Column(db.models.TextField(("")), nullable=True)
+    existing_risk_control = db.Column(db.Text, nullable=True)
+    additional_risk_control = db.Column(db.Text, nullable=True)
     severity = db.Column(db.Integer, nullable=False)
     likelihood = db.Column(db.Integer, nullable=False)
     risk_rating = db.Column(db.Integer, nullable=False)
@@ -74,7 +74,7 @@ class RA_team_member(db.Model):
     __tablename__ = 'RA_team_member'
 
     # Define fields
-    RA_team_id = db.Column(db.Integer, db.ForeignKey('RA_team.RA_team_id'), nullable=False)
+    RA_team_id = db.Column(db.Integer, db.ForeignKey('RA_team.RA_team_id'),primary_key=True, nullable=False)
     RA_team_member = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
 
 class share_access(db.Model):
