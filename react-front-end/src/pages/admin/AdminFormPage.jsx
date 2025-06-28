1e3c6ff55005b0d3dd7656e825ba4cac2b800435
marcusayay <marcusayay@gmail.com>
2025-06-28 18:38:19 +0800

    Updated Card2

--- react-front-end/src/pages/admin/AdminFormPage.jsx
+++ react-front-end/src/pages/admin/AdminFormPage.jsx
@@ -5,6 +5,7 @@ import CTAButton from "../../components/CTAButton.jsx";
 import SearchBar from "../../components/SearchBar.jsx";
 import FormCardA from "../../components/FormCardA.jsx";
 import FormCardB from "../../components/FormCardB.jsx";
+import FormCardA2 from "../../components/FormCardA2.jsx";
 
 
 export default function AdminForm() {
@@ -78,6 +79,46 @@ export default function AdminForm() {
                 owner="John Doe"
                 tags={["Pending"]}
                 onShare={() => console.log("Share")}
+                onDownload={() => console.log("Download")}
+                onDelete={() => console.log("Delete")}
+              />
+        </div>
+
+        {/* Style A2 */}
+        <div className="mt-5 grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-6 my-6 w-full">
+              <FormCardA2
+                status="Ongoing"
+                date="19/05/2025"
+                title="Advanced Techniques in Radiation Therapy for Oncology Patients"
+                owner="Jane Doe"
+                expiryDate="-"
+                onView={() => console.log("View")}
+                onShare={() => console.log("Share")}
+                onDuplicate={() => console.log("Duplicate")}
+                onDownload={() => console.log("Download")}
+                onDelete={() => console.log("Delete")}
+              />
+              <FormCardA2
+                status="Pending"
+                date="20/05/2025"
+                title="Innovative Approaches to Radiation Dose Optimization in Pediatric Imaging"
+                owner="Emily Johnson"         
+                expiryDate="-"
+                onView={() => console.log("View")}
+                onShare={() => console.log("Share")}
+                onDuplicate={() => console.log("Duplicate")}
+                onDownload={() => console.log("Download")}
+                onDelete={() => console.log("Delete")}
+              />
+              <FormCardA2
+                status="Completed"
+                date="21/05/2025"   
+                title="Radiation Safety Protocols for Interventional Radiology Procedures"
+                owner="Michael Brown"
+                expiryDate="24/06/2025"
+                onView={() => console.log("View")}
+                onShare={() => console.log("Share")}
+                onDuplicate={() => console.log("Duplicate")}
                 onDownload={() => console.log("Download")}
                 onDelete={() => console.log("Delete")}
               />
